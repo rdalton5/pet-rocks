4c26f3e317b06677000ae62d6fa7bd475f7f0c78
Robert Dalton <rdalton5@kent.edu>
2021-04-14 21:41:06 -0400

    Add a resource route for the pet rock controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetRockController;
+use App\Models\PetRock;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/petrock');
 });
+
+Route::resource('petrock', PetRockController::class);
